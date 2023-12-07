28bf09a6a2bf8f9450d17821f06c1bfa536b2934
traviscarter24 <traviscarter@nevada.unr.edu>
2023-12-06 21:36:12 -0800

    Added inventory/item/interact scripts for future

---
+++ capstoneProject/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+    public List<Item> inventory = new List<Item>(); // List to store collected items
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Add item to the inventory
+    public void AddItem(Item item)
+    {
+        inventory.Add(item);
+        // You can add further logic here, like UI updates, etc.
+    }
+
+    // Remove item from the inventory
+    public void RemoveItem(Item item)
+    {
+        inventory.Remove(item);
+        // You can add further logic here, like UI updates, etc.
+    }
+}
