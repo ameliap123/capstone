ec8293c19de6ad6d48515d5e6dc028022bf18f09
Arabian-Mikayla <marabian2480@nevada.unr.edu>
2024-02-14 21:35:33 -0800

    quest manager script changes

--- capstoneProject/Assets/Scripts/Quests/QuestManager.cs
+++ capstoneProject/Assets/Scripts/Quests/QuestManager.cs
@@ -26,6 +26,42 @@ public class QuestManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    public void QuestRequest(QuestObject thisQuestObject)
+    {
+        //Check for Available Quests
+        if (thisQuestObject.availableQuestIDs.Count > 0)
+        {
+            for (int i = 0; i < questList.Count; i++)
+            {
+                for (int j = 0; j < thisQuestObject.availableQuestIDs.Count; j++)
+                {
+                    if (questList[i].id == thisQuestObject.availableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.AVAILABLE)
+                    {
+                        Debug.Log("Quest ID: " + thisQuestObject.availableQuestIDs[j] + " " + questList[i].progress);
+                        //Testing
+                        AcceptQuest(thisQuestObject.availableQuestIDs[j]);
+                        //quest ui manager
+                    }
+                }
+            }
+
+        }
+        //Check for Active Quests
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+            for (int j = 0; j < thisQuestObject.receivableQuestIDs.Count; j++)
+            {
+                if (currentQuestList[i].id == thisQuestObject.receivableQuestIDs[j] && currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED || currentQuestList[i].progress == Quest.QuestProgress.COMPLETE)
+                {
+                    Debug.Log("Quest ID: " + thisQuestObject.receivableQuestIDs[j] + " is " + currentQuestList[i].progress);
+
+                    CompleteQuest(thisQuestObject.receivableQuestIDs[j]);
+                    //quest ui manager
+                }
+            }
+        }
+    }
+
     //Accept a quest
     public void AcceptQuest(int questID)
     {
@@ -62,9 +98,39 @@ public class QuestManager : MonoBehaviour
             {
                 currentQuestList[i].progress = Quest.QuestProgress.DONE;
                 currentQuestList.Remove(currentQuestList[i]);
+
+                //reward
+            }
+        }
+        //check for chain quests (definitely could do tutorial like this)
+        CheckChainQuest(questID);
+    }
+
+    //Check Chain Quests
+    //not pubic, but can be made private outside the simple void function
+    void CheckChainQuest(int questID)
+    {
+        int num = 0;
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].nextQuest > 0)
+            {
+                num = questList[i].nextQuest;
+            }
+        }
+
+        if (num > 0)
+        {
+            for (int i = 0; i < questList.Count; i++)
+            {
+                if (questList[i].id == num && questList[i].progress == Quest.QuestProgress.NOT_AVAILABLE)
+                {
+                    questList[i].progress = Quest.QuestProgress.AVAILABLE;
+                }
             }
         }
     }
+
 
     //Add Items
     public void AddQuestItem(string questObjective, int itemAmount)
@@ -123,4 +189,53 @@ public class QuestManager : MonoBehaviour
         }
         return false;
     }
+
+
+    //BOOLS 2 - passing over complete quest object, not ID like before 
+
+    public bool CheckAvailableQuests(QuestObject thisQuestObject)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            for (int j = 0; j < thisQuestObject.availableQuestIDs.Count; j++)
+            {
+                if (questList[i].id == thisQuestObject.availableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.AVAILABLE)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool CheckAcceptedQuests(QuestObject thisQuestObject)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            for (int j = 0; j < thisQuestObject.receivableQuestIDs.Count; j++)
+            {
+                if (questList[i].id == thisQuestObject.receivableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.ACCEPTED)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool CheckCompletedQuests(QuestObject thisQuestObject)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            for (int j = 0; j < thisQuestObject.receivableQuestIDs.Count; j++)
+            {
+                if (questList[i].id == thisQuestObject.receivableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.COMPLETE)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
