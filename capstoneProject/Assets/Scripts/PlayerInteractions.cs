8b4fb9e5d7e8d25374a525eb2b2745a3596b01e0
cpe400 <78009951+ameliap123@users.noreply.github.com>
2024-03-03 10:15:11 -0800

    FIxed water collider and item count

--- capstoneProject/Assets/Scripts/PlayerInteractions.cs
+++ capstoneProject/Assets/Scripts/PlayerInteractions.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour
 {
     public InventoryManager inventoryManager;
     public Item waterItem;
+    public TMP_Text waterItemCountText; // Reference to the TextMeshPro text component
+    public TMP_Text interactionPromptText; // Reference to the TextMeshPro text component for interaction prompt
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered trigger area");
+        if (collision.CompareTag("WaterBody"))
+        {
+            interactionPromptText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exited trigger area");
+        if (collision.CompareTag("WaterBody"))
+        {
+            interactionPromptText.gameObject.SetActive(false);
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -14,9 +34,19 @@ public class PlayerInteractions : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                // Collect water
+                //collect water
                 inventoryManager.AddItem(waterItem, 1);
+
+                // Update the TextMeshPro text component
+                UpdateWaterItemCountText();
+
+                Debug.Log("Collected 1 water item");
             }
         }
+    }
+    private void UpdateWaterItemCountText()
+    {
+        int waterItemCount = inventoryManager.GetItemQuantity(waterItem);
+        waterItemCountText.text = waterItemCount.ToString();
     }
 }
